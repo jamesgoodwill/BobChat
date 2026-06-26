74b03e1cd7f059f9ed588d7998b1b67f39e83d8e
Jim Goodwill <jgoodwill@emergent.tech>
2026-06-26 15:39:18 -0500

    initial commit

---
+++ BobChat/BobChatApp.swift
@@ -0,0 +1,17 @@
+//
+//  BobChatApp.swift
+//  BobChat
+//
+//  Created by James Goodwill on 6/26/26.
+//
+
+import SwiftUI
+
+@main
+struct BobChatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BobChat/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BobChat
+//
+//  Created by James Goodwill on 6/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, Bob!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
